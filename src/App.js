138b92cdc1d267892f3a363d43f07cd6383f5fc5
Sam Codrington <sam.codrington@queensu.ca>
2019-10-15 21:32:48 -0500

    intialize map to view of Canada

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Helmet} from 'react-helmet';
+
+//import containers
+import Map from './containers/Map';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Helmet>
+				<link rel="stylesheet" href="https://unpkg.com/leaflet@1.5.1/dist/leaflet.css"
+					integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ=="
+					crossorigin=""/>
+				<script src="https://unpkg.com/leaflet@1.5.1/dist/leaflet.js"
+					integrity="sha512-GffPMF3RvMeYyc1LWMHtK8EbPv0iNZ8/oTtHPx9/cc2ILxQ+u905qIwdpULaqDkyBKgOaB57QTMg7ztg8Jm2Og=="
+					crossorigin=""/>
+			</Helmet>
+			<div className = "leaflet-container">
+				<Map/>
+			</div>
+		</div>
+    
+	);
 }
 
 export default App;
